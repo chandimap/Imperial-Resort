2b02a0f328a31016e980d9915dcb2188408fcd4b
Chandima Ranasinghe <chandimapr@gmail.com>
2020-08-27 17:47:45 +0530

    Updates

---
+++ precache-manifest.3d3d9ac94d3c2cb8e861a01304f8ac68.js
@@ -0,0 +1,246 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec6082720804904c39ba3ff7cc64dc20",
+    "url": "/imperial-resort/index.html"
+  },
+  {
+    "revision": "96ffaa4773379e056ad2",
+    "url": "/imperial-resort/static/css/main.18fd0c07.chunk.css"
+  },
+  {
+    "revision": "0955a9d4cdefa7cd1fbf",
+    "url": "/imperial-resort/static/js/2.eacfad56.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/imperial-resort/static/js/2.eacfad56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96ffaa4773379e056ad2",
+    "url": "/imperial-resort/static/js/main.b2cef2ec.chunk.js"
+  },
+  {
+    "revision": "1e361092a267638db20d",
+    "url": "/imperial-resort/static/js/runtime-main.3734af31.js"
+  },
+  {
+    "revision": "55fb1c855a018cce78d32cbd584bcc37",
+    "url": "/imperial-resort/static/media/double_basic.55fb1c85.jpg"
+  },
+  {
+    "revision": "2361a921a61d8fcb00745c07fafca90f",
+    "url": "/imperial-resort/static/media/double_basic1.2361a921.jpg"
+  },
+  {
+    "revision": "efe75dabfb71b4a5cf2b22a0c1171136",
+    "url": "/imperial-resort/static/media/double_basic2.efe75dab.jpg"
+  },
+  {
+    "revision": "d2bb07ef704e8997ab65757174308061",
+    "url": "/imperial-resort/static/media/double_basic3.d2bb07ef.jpg"
+  },
+  {
+    "revision": "2dc5c6bb2b219ad204d5fc272d9a7f43",
+    "url": "/imperial-resort/static/media/double_deluxe.2dc5c6bb.jpg"
+  },
+  {
+    "revision": "159c9bad6ade8bd4062b967233b341a5",
+    "url": "/imperial-resort/static/media/double_deluxe1.159c9bad.jpg"
+  },
+  {
+    "revision": "1d012c5789ab4fb96344a90a354bdb7b",
+    "url": "/imperial-resort/static/media/double_deluxe2.1d012c57.jpg"
+  },
+  {
+    "revision": "4fe7e2cee57e4103d1afb72aed35d198",
+    "url": "/imperial-resort/static/media/double_deluxe3.4fe7e2ce.jpg"
+  },
+  {
+    "revision": "fd10facb0330deff1bb1ea172dc81267",
+    "url": "/imperial-resort/static/media/double_economy.fd10facb.jpg"
+  },
+  {
+    "revision": "1a3334bfe7545c8cedb4d9ef3a1dac4e",
+    "url": "/imperial-resort/static/media/double_economy1.1a3334bf.jpg"
+  },
+  {
+    "revision": "5208bf60d9d10372063116e3aa8deb2d",
+    "url": "/imperial-resort/static/media/double_economy2.5208bf60.jpg"
+  },
+  {
+    "revision": "16b635084f9090a0225422cf0d59b4d8",
+    "url": "/imperial-resort/static/media/double_economy3.16b63508.jpg"
+  },
+  {
+    "revision": "033e9da9c1ef4c9c63ad90478a9541bd",
+    "url": "/imperial-resort/static/media/double_standard.033e9da9.jpg"
+  },
+  {
+    "revision": "c779fcbb53031e90177fb1f7702806e8",
+    "url": "/imperial-resort/static/media/double_standard1.c779fcbb.jpg"
+  },
+  {
+    "revision": "14361d1ee5b8fe3eaf620f8a96170b9a",
+    "url": "/imperial-resort/static/media/double_standard2.14361d1e.jpg"
+  },
+  {
+    "revision": "2b3f386cfb08b8033de16ef7bec838f8",
+    "url": "/imperial-resort/static/media/double_standard3.2b3f386c.jpg"
+  },
+  {
+    "revision": "1fa6b4e922d3c477ca35ff025706d6dd",
+    "url": "/imperial-resort/static/media/family_basic.1fa6b4e9.jpg"
+  },
+  {
+    "revision": "55e92165ee78ddbfdfbc2b237f5d922a",
+    "url": "/imperial-resort/static/media/family_basic1.55e92165.jpg"
+  },
+  {
+    "revision": "64ebb09a78c579b9b4dcbaaf78e53957",
+    "url": "/imperial-resort/static/media/family_basic2.64ebb09a.jpg"
+  },
+  {
+    "revision": "efe75dabfb71b4a5cf2b22a0c1171136",
+    "url": "/imperial-resort/static/media/family_basic3.efe75dab.jpg"
+  },
+  {
+    "revision": "b3a7483e26650a7bed588186ff982540",
+    "url": "/imperial-resort/static/media/family_deluxe.b3a7483e.jpg"
+  },
+  {
+    "revision": "ce04fd258708dbe3cf8b6e59994f5473",
+    "url": "/imperial-resort/static/media/family_deluxe1.ce04fd25.jpg"
+  },
+  {
+    "revision": "212f6bcb9c77832c5ebd17324828c5d1",
+    "url": "/imperial-resort/static/media/family_deluxe2.212f6bcb.jpg"
+  },
+  {
+    "revision": "2e579395c6d95bd584584c812729d196",
+    "url": "/imperial-resort/static/media/family_deluxe3.2e579395.jpg"
+  },
+  {
+    "revision": "5879fa7040caafcd35ec6aa3bb218915",
+    "url": "/imperial-resort/static/media/family_economy.5879fa70.jpg"
+  },
+  {
+    "revision": "e70a926b87c1b88cf92784e55d67a0ae",
+    "url": "/imperial-resort/static/media/family_economy1.e70a926b.jpg"
+  },
+  {
+    "revision": "9641eceb45ee6dde6ca832533eb5e894",
+    "url": "/imperial-resort/static/media/family_economy2.9641eceb.jpg"
+  },
+  {
+    "revision": "c4ad89bfc25ef6bb70cf63b873643a47",
+    "url": "/imperial-resort/static/media/family_economy3.c4ad89bf.jpg"
+  },
+  {
+    "revision": "c2109ac20599ee76f7767f3731e6209d",
+    "url": "/imperial-resort/static/media/family_standard.c2109ac2.jpg"
+  },
+  {
+    "revision": "75f3db811687827d23b5ad898b520e0f",
+    "url": "/imperial-resort/static/media/family_standard1.75f3db81.jpg"
+  },
+  {
+    "revision": "a99164493758bacbfc249618b6bcaea6",
+    "url": "/imperial-resort/static/media/family_standard2.a9916449.jpg"
+  },
+  {
+    "revision": "e7af1fc2233b743c375c57a1e821d21f",
+    "url": "/imperial-resort/static/media/family_standard3.e7af1fc2.jpg"
+  },
+  {
+    "revision": "12582f039735ddeef15c336fc1a057f6",
+    "url": "/imperial-resort/static/media/img-bcg.12582f03.jpg"
+  },
+  {
+    "revision": "5f85d8c8633d50d4ca675895e5ce5508",
+    "url": "/imperial-resort/static/media/presidential_suite.5f85d8c8.jpg"
+  },
+  {
+    "revision": "06d4afd194bdbd62016c6205cedc99d7",
+    "url": "/imperial-resort/static/media/presidential_suite1.06d4afd1.jpg"
+  },
+  {
+    "revision": "633a514c421d49d720e522a2c7b9b243",
+    "url": "/imperial-resort/static/media/presidential_suite2.633a514c.jpg"
+  },
+  {
+    "revision": "c8da8ba2b4d0065bfa2812a8f4e4fa09",
+    "url": "/imperial-resort/static/media/presidential_suite3.c8da8ba2.jpg"
+  },
+  {
+    "revision": "682268ed3093fb67cc126019763d762c",
+    "url": "/imperial-resort/static/media/room-2.682268ed.jpg"
+  },
+  {
+    "revision": "41ad9edc77028127b3cb956dff861504",
+    "url": "/imperial-resort/static/media/room-9.41ad9edc.jpg"
+  },
+  {
+    "revision": "c052636e01794b6c4d096fc71275814c",
+    "url": "/imperial-resort/static/media/single_basic.c052636e.jpg"
+  },
+  {
+    "revision": "54eb9b872688c6e91b974649c95becbf",
+    "url": "/imperial-resort/static/media/single_basic1.54eb9b87.jpg"
+  },
+  {
+    "revision": "85aa9d5d0bf17b2e8d598a18ef4fb9dc",
+    "url": "/imperial-resort/static/media/single_basic2.85aa9d5d.jpg"
+  },
+  {
+    "revision": "72d086389535dc4b737662ba98fab1bb",
+    "url": "/imperial-resort/static/media/single_basic3.72d08638.jpg"
+  },
+  {
+    "revision": "a250a9aa4bea392725809810dd75a9db",
+    "url": "/imperial-resort/static/media/single_deluxe.a250a9aa.jpg"
+  },
+  {
+    "revision": "c9d412453887f565c770a6d2d5009f63",
+    "url": "/imperial-resort/static/media/single_deluxe1.c9d41245.jpg"
+  },
+  {
+    "revision": "b4c8a177e6bb21a1e74a93d0ef2a6ece",
+    "url": "/imperial-resort/static/media/single_deluxe2.b4c8a177.jpg"
+  },
+  {
+    "revision": "e16dffe42ba2747e462837261641f1c7",
+    "url": "/imperial-resort/static/media/single_deluxe3.e16dffe4.jpg"
+  },
+  {
+    "revision": "82f6901daf2e7ba7fa0032310234ce2d",
+    "url": "/imperial-resort/static/media/single_economy.82f6901d.jpg"
+  },
+  {
+    "revision": "f9dd01302cd6dab8482f8875f5bdcb6c",
+    "url": "/imperial-resort/static/media/single_economy1.f9dd0130.jpg"
+  },
+  {
+    "revision": "14beb580a81a97dbbbe35ad576ae3f97",
+    "url": "/imperial-resort/static/media/single_economy2.14beb580.jpg"
+  },
+  {
+    "revision": "03db8bea30b6b5763bba6b40a22eef7a",
+    "url": "/imperial-resort/static/media/single_economy3.03db8bea.jpg"
+  },
+  {
+    "revision": "ddeb346998db42a4ce6588e6c223c2da",
+    "url": "/imperial-resort/static/media/single_standard.ddeb3469.jpg"
+  },
+  {
+    "revision": "c4dde6185d3d8e0199b016265a5732e0",
+    "url": "/imperial-resort/static/media/single_standard1.c4dde618.jpg"
+  },
+  {
+    "revision": "5f279936d0988ca6a88678bae183067f",
+    "url": "/imperial-resort/static/media/single_standard2.5f279936.jpg"
+  },
+  {
+    "revision": "5c012b244c551fc6a0282651f7c88e22",
+    "url": "/imperial-resort/static/media/single_standard3.5c012b24.jpg"
+  }
+]);
